7b6b897478a933648d59cd7e79feba13ec0c6ca8
karl <karl.michalik@gmail.com>
2021-10-19 18:38:01 +0200

    *fizzbuzz re-grouped *divided to hide answer

---
+++ Interview_Exercises/Exercise1/Classic_FizzBuzz_description.py
@@ -0,0 +1,6 @@
+"""
+Classic FizzBuzz exercise.
+For range from 1 to 100
+if number is divisible by 3 print "fizz", if divisible by 5 print "buzz"
+and if divisible by both 3 and 5 print "FizzBuzz"
+"""
